62629d255134333d9dc246c2f506616e8f65b98d
Huy Hoàng Vũ <hoangvu130301@gmail.com>
2023-02-03 14:25:23 +0700

    first-commit-game-bau-cua

---
+++ src/TomCuaCa/Baucua.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import DanhSachQuanCuoc from './DanhSachCuoc/DanhSachQuanCuoc'
+import DanhSachXucXac from './DanhSachCuoc/DanhSachXucXac'
+  
+export default function Baucua() {
+  return (
+    <div className='container-fluid'>
+      <div className='text-center container pb-1'>
+        <p style={{ fontSize: '35px', fontWeight: 'bold', marginTop: '50px' }}>GAME BẦU CUA</p>
+      </div>
+      <div className='row container m-auto'>
+        <div className='col-12 d-flex justify-content-center'> <DanhSachQuanCuoc/> </div>
+        <div className='col-12 d-flex justify-content-center' style={{marginBottom: '20px'}}> <DanhSachXucXac/> </div>
+      </div>
+    </div>
+  )
+}
